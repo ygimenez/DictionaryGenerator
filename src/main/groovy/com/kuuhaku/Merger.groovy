38c8a4f83c13f16e9967cadb750e49915dd5368e
Ygimenez <yagogimenez1@hotmail.com>
2024-07-26 17:52:33 -0300

    Initial commit

---
+++ src/main/groovy/com/kuuhaku/Merger.groovy
@@ -0,0 +1,68 @@
+package com.kuuhaku
+
+import org.apache.commons.lang3.StringUtils
+
+void dropDeadends(Collection<String> words) {
+	def correct = false
+	int prcnt = 0
+
+	parent: while (!correct) {
+		for (final i in 0..<words.size()) {
+			def word = words[i]
+			def ending = word.substring(word.length() - 2)
+			if (words.count { it.startsWith(ending) } < 3) {
+				words.removeIf {
+					if (it.endsWith(ending)) {
+						println "Dropped $it"
+						return true
+					}
+
+					return false
+				}
+
+				prcnt = 0
+				continue parent
+			}
+
+			int curr = i * 100 / words.size() as int
+			if (curr > prcnt) {
+				prcnt = curr
+				println "Processing... $i/${words.size()} ($curr%)"
+			}
+		}
+
+		correct = true
+	}
+}
+
+def dir = new File("dicts")
+if (!dir.exists() || !dir.directory) return
+
+def dict = new File("dict_full.txt")
+if (!dict.exists() || !dict.file) dict.createNewFile()
+
+def groups = [:] as Map<String, List<String>>
+def words = [] as Set<String>
+dir.listFiles().each {
+	it.eachLine {
+		def (word, act) = it.split(",") as List<String>
+
+		word = StringUtils.stripAccents(word.split(" ")[1]).toLowerCase()
+		if (word.contains("-") || word.length() < 3) return
+
+		groups.computeIfAbsent(act, k -> []) << word
+	}
+
+	def lemma = groups["lemma"]
+	def fna = groups["fna"]
+	if (fna) {
+		lemma.removeAll(fna)
+	}
+
+	println "Joined $it"
+	words.addAll(groups["lemma"])
+}
+
+dropDeadends(words)
+
+dict.text = words.join("\n")
