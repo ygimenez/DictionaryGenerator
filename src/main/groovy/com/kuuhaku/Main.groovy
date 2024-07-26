0dc7de7b86b0f913fd0c4a9b4f265a024b191b94
Ygimenez <yagogimenez1@hotmail.com>
2024-07-26 19:28:41 -0300

    Update

--- src/main/groovy/com/kuuhaku/Main.groovy
+++ src/main/groovy/com/kuuhaku/Main.groovy
@@ -1,48 +1,54 @@
 package com.kuuhaku
 
-def baseURL = "https://voc.iilp.cplp.org/index.php?action=simplesearch&base=form"
-def letters = "a".."z"
+class Main {
+	static void main(String[] args) {
+		def baseURL = "https://voc.iilp.cplp.org/index.php?action=simplesearch&base=form"
+		def letters = "a".."z"
 
-def extract = (ch, i) -> {
-	def content =  "$baseURL&query=$ch&sel=start&&start=$i".toURL().text
-	int total = 100
-	if (content =~ / \(de (\d+)\)<\/i>/) {
-		total = (content =~ / \(de (\d+)\)<\/i>/)[0][1] as int
-	}
+		def extract = (ch, i) -> {
+			def content =  "$baseURL&query=$ch&sel=start&&start=$i".toURL().text
+			int total = 100
+			if (content =~ / \(de (\d+)\)<\/i>/) {
+				total = (content =~ / \(de (\d+)\)<\/i>/)[0][1] as int
+			}
 
-	def words = []
-	(content =~ /<a class=let href="\?action=(\w+?)&.+?><b>(.+?)<\/b>.+?>(\w+)<\/span>/).each {
-		def act = it[1]
-		def word = it[2]
-		def type = it[3]
+			def words = []
+			(content =~ /<a class=let href="\?action=(\w+?)&.+?><b>(.+?)<\/b>.+?>(\w+)<\/span>/).each {
+				def act = it[1]
+				def word = it[2]
+				def type = it[3]
 
-		words << "$word,$act,$type"
-	}
+				words << "$word,$act,$type"
+			}
 
-	return [words, total]
-}
-
-def dir = new File("dicts")
-if (!dir.exists() || !dir.directory) dir.mkdir()
-
-letters.each { ch ->
-	def dump = new File(dir, "dict_${ch}.txt")
-	if (dump.exists()) return
-	dump.createNewFile()
-
-	def (words, total) = extract(ch, 0)
-	words.each {
-		dump.append "$it\n"
-	}
-
-	println "Progress (${ch.toUpperCase()}) - 100 of $total (${100 * 100 / total as int}%)"
-	(Math.ceil(total / 100) - 1).times {
-		int page = 100 + 100 * it
-		extract(ch, page)[0].each {
-			dump.append "$it\n"
+			return [words, total]
 		}
 
-		println "Progress (${ch.toUpperCase()}) - ${100 + page} of $total (${(100 + page) * 100 / total as int}%)"
-		sleep 100
+		def dir = new File("dicts")
+		if (!dir.exists() || !dir.directory) dir.mkdir()
+
+		letters.each { ch ->
+			def dump = new File(dir, "dict_${ch}.txt")
+			if (dump.exists()) return
+			dump.createNewFile()
+
+			def (words, total) = extract(ch, 0)
+			words.each {
+				dump.append "$it\n"
+			}
+
+			println "Progress (${ch.toUpperCase()}) - 100 of $total (${100 * 100 / total as int}%)"
+			(Math.ceil(total / 100) - 1).times {
+				int page = 100 + 100 * it
+				extract(ch, page)[0].each {
+					dump.append "$it\n"
+				}
+
+				println "Progress (${ch.toUpperCase()}) - ${100 + page} of $total (${(100 + page) * 100 / total as int}%)"
+				sleep 100
+			}
+		}
+
+		new Merger()
 	}
 }
--- src/main/groovy/com/kuuhaku/Merger.groovy
+++ src/main/groovy/com/kuuhaku/Merger.groovy
@@ -2,67 +2,71 @@ package com.kuuhaku
 
 import org.apache.commons.lang3.StringUtils
 
-void dropDeadends(Collection<String> words) {
-	def correct = false
-	int prcnt = 0
+class Merger {
+	Merger() {
+		def dir = new File("dicts")
+		if (!dir.exists() || !dir.directory) return
 
-	parent: while (!correct) {
-		for (final i in 0..<words.size()) {
-			def word = words[i]
-			def ending = word.substring(word.length() - 2)
-			if (words.count { it.startsWith(ending) } < 3) {
-				words.removeIf {
-					if (it.endsWith(ending)) {
-						println "Dropped $it"
-						return true
-					}
+		def dict = new File("dict_full.txt")
+		if (!dict.exists() || !dict.file) dict.createNewFile()
 
-					return false
-				}
+		def groups = [:] as Map<String, List<String>>
+		def words = [] as Set<String>
+		dir.listFiles().each {
+			it.eachLine {
+				def (word, act) = it.split(",") as List<String>
 
-				prcnt = 0
-				continue parent
+				word = StringUtils.stripAccents(word.split(" ")[1]).toLowerCase()
+				if (word.contains("-") || word.length() < 3) return
+
+				groups.computeIfAbsent(act, k -> []) << word
 			}
 
-			int curr = i * 100 / words.size() as int
-			if (curr > prcnt) {
-				prcnt = curr
-				println "Processing... $i/${words.size()} ($curr%)"
+			def lemma = groups["lemma"]
+			def fna = groups["fna"]
+			if (fna) {
+				lemma.removeAll(fna)
 			}
+
+			println "Joined $it"
+			words.addAll(groups["lemma"])
 		}
 
-		correct = true
+		dropDeadends(words)
+
+		dict.text = words.join("\n")
+	}
+
+	static void dropDeadends(Collection<String> words) {
+		def correct = false
+		int prcnt = 0
+
+		parent: while (!correct) {
+			for (final i in 0..<words.size()) {
+				def word = words[i]
+				def ending = word.substring(word.length() - 2)
+				if (words.count { it.startsWith(ending) } < 3) {
+					words.removeIf {
+						if (it.endsWith(ending)) {
+							println "Dropped $it"
+							return true
+						}
+
+						return false
+					}
+
+					prcnt = 0
+					continue parent
+				}
+
+				int curr = i * 100 / words.size() as int
+				if (curr > prcnt) {
+					prcnt = curr
+					println "Processing... $i/${words.size()} ($curr%)"
+				}
+			}
+
+			correct = true
+		}
 	}
 }
-
-def dir = new File("dicts")
-if (!dir.exists() || !dir.directory) return
-
-def dict = new File("dict_full.txt")
-if (!dict.exists() || !dict.file) dict.createNewFile()
-
-def groups = [:] as Map<String, List<String>>
-def words = [] as Set<String>
-dir.listFiles().each {
-	it.eachLine {
-		def (word, act) = it.split(",") as List<String>
-
-		word = StringUtils.stripAccents(word.split(" ")[1]).toLowerCase()
-		if (word.contains("-") || word.length() < 3) return
-
-		groups.computeIfAbsent(act, k -> []) << word
-	}
-
-	def lemma = groups["lemma"]
-	def fna = groups["fna"]
-	if (fna) {
-		lemma.removeAll(fna)
-	}
-
-	println "Joined $it"
-	words.addAll(groups["lemma"])
-}
-
-dropDeadends(words)
-
-dict.text = words.join("\n")
